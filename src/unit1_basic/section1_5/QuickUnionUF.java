64a8a47773716f9518dd8f9b56b88cb51619e34d
beta <541737447@qq.com>
2018-06-26 16:56:43 +0800

    quickunion算法

--- src/unit1_basic/section1_5/QuickUnionUF.java
+++ src/unit1_basic/section1_5/QuickUnionUF.java
@@ -3,54 +3,47 @@ package unit1_basic.section1_5;
 import java.util.LinkedList;
 import java.util.Queue;
 
-/**
- * quick-find 并查集
- * @author beta
- *
- */
-public class QuickFindUF implements UF {
+public class QuickUnionUF implements UF{
 
-	private int[] id;
 	private int count;
+	private int[] arr;
 	
-	public QuickFindUF(int n) {
-		id = new int[n];
-		for(int i=0;i<n;i++) {
-			id[i] = i;
+	public QuickUnionUF(int n) {
+		arr = new int[n];
+		for(int i=0;i<arr.length;i++) {
+			arr[i] = i;
 		}
 		count = n;
 	}
 	
 	@Override
 	public void union(int p, int q) {
-		if(find(p)!= find(q)) {
-			int n = find(p);
-			for(int i=0;i<id.length;i++) {
-				if(find(i)==n) {
-					id[i] = find(q);
-				}
-			}
+		if(find(p)!=find(q)) {
+			arr[find(p)] = find(q);
 			count--;
 		}
 	}
 
 	@Override
 	public int find(int p) {
-		return id[p];
+		while(p!=arr[p]) {
+			p = arr[p];
+		}
+		return p;
 	}
 
 	@Override
 	public boolean connected(int p, int q) {
-		return find(p) == find(q);
+		return find(p)==find(q);
 	}
 
 	@Override
 	public int count() {
 		return count;
 	}
-	
+
 	public static void main(String[] args) {
-		QuickFindUF uf = new QuickFindUF(10);
+		QuickUnionUF uf = new QuickUnionUF(10);
 		
 		Queue<Integer> m = new LinkedList<>();
 		m.add(43);
