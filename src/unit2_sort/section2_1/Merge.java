f515720911ab2de8dec66a5f35d8d914d98b9b50
beta <541737447@qq.com>
2018-06-30 09:34:24 +0800

    自顶向下归并排序

--- src/unit2_sort/section2_1/Merge.java
+++ src/unit2_sort/section2_1/Merge.java
@@ -8,38 +8,61 @@ import java.util.Arrays;
  *
  */
 public class Merge {
+	//辅助函数
+	private static char[] aux;
+	
 	//原地归并
 	public static void merge(char[] arr,int l,int mid,int r) {
 		int i = l;
 		int j = mid+1;
-		char[] a = new char[arr.length];
-		for(int k=0;k<arr.length;k++) {
-			a[k] = arr[k];
+		
+		for(int k=l;k<=r;k++) {
+			aux[k] = arr[k];
 		}
 		
-		for(int n=0;n<a.length;n++) {
+		for(int n=l;n<=r;n++) {
 			if(i>mid) {
 				//左侧数组用尽
-				arr[n] = a[j];
+				arr[n] = aux[j];
 				j++;
 			}else if(j>r) {
 				//右侧数组用尽
-				arr[n] = a[i];
+				arr[n] = aux[i];
 				i++;
-			}else if(a[i]<a[j]) {
-				arr[n] = a[i];
+			}else if(aux[i]<aux[j]) {
+				arr[n] = aux[i];
 				i++;
 			}else {
-				arr[n] = a[j];
+				arr[n] = aux[j];
 				j++;
 			}
-			System.out.println(Arrays.toString(arr));
 		}
 	}
 	
+	//自上到下归并排序
+	public static void sort(char[] arr) {
+		aux = new char[arr.length];
+		sort(arr,0,arr.length-1);
+	}
+	
+	private static void sort(char[] arr, int l, int r) {
+		if(l==r) {
+			return ;
+		}
+		
+		int mid = l+(r-l)/2;
+		sort(arr, l, mid);
+		sort(arr, mid+1, r);
+		merge(arr, l, mid, r);
+	}
+
 	public static void main(String[] args) {
-		char[] arr = {'e','e','g','m','r','a','c','e','r','t'};
-		merge(arr, 0, (arr.length-1)/2, arr.length-1);
-		System.out.println(Arrays.toString(arr));
+//		char[] arr = {'e','e','g','m','r','a','c','e','r','t'};
+//		merge(arr, 0, (arr.length-1)/2, arr.length-1);
+//		System.out.println(Arrays.toString(arr));
+		
+		char[] a = {'m','e','r','g','e','s','o','r','t','e','x','a','m','p','l','e'};
+		sort(a);
+		System.out.println(Arrays.toString(a));
 	}
 }
